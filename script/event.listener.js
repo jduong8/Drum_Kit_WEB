ff9bbfc5f0b19ae7810541012668899a5defcf21
Duong Jonathan <djonathan91.77@gmail.com>
2024-06-14 10:14:46 +0200

    Initial drum kit project

---
+++ script/event.listener.js
@@ -0,0 +1,11 @@
+var keys = [81, 83, 68, 70, 71, 72, 74, 75, 76]
+
+this.addEventListener('keydown', e => {
+
+    var keyID = e.keyCode ? e.keyCode : e.which;
+
+    if (keys.includes(keyID)) {
+        var sound = document.getElementById(keyID)
+        sound.play();
+    }
+})
