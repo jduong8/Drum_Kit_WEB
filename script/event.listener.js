53528f7404e2e9879f0205537f5ada0123d89844
Duong Jonathan <djonathan91.77@gmail.com>
2024-06-14 10:26:40 +0200

    tech: Added a transition end event listener to remove the .playing class after the transition ends. - Using a generic .playing class to simplify CSS and JavaScript code. quality: add doc

--- script/event.listener.js
+++ script/event.listener.js
@@ -1,11 +1,35 @@
-var keys = [81, 83, 68, 70, 71, 72, 74, 75, 76]
+// Liste des codes de touches qui sont écoutés par le script.
+const keys = [81, 83, 68, 70, 71, 72, 74, 75, 76];
 
-this.addEventListener('keydown', e => {
+// Ajout d'un écouteur d'événement pour l'événement 'keydown' sur l'objet 'window'.
+// Cet événement se déclenche chaque fois qu'une touche du clavier est enfoncée.
+window.addEventListener('keydown', e => {
 
-    var keyID = e.keyCode ? e.keyCode : e.which;
+  // Récupère le code de la touche enfoncée.
+  // 'e.keyCode' est utilisé pour les navigateurs plus anciens et 'e.which' est une alternative.
+  const keyID = e.keyCode ? e.keyCode : e.which;
 
-    if (keys.includes(keyID)) {
-        var sound = document.getElementById(keyID)
-        sound.play();
+  // Vérifie si le code de la touche enfoncée fait partie du tableau 'keys'.
+  if (keys.includes(keyID)) {
+    // Récupère l'élément audio dont l'ID correspond au code de la touche enfoncée.
+    const sound = document.getElementById(keyID);
+    // Récupère l'élément 'div' avec la classe 'key' et l'attribut 'data-key' correspondant au code de la touche enfoncée.
+    const key = document.querySelector(`.key[data-key="${keyID}"]`);
+
+    // Si l'élément audio existe, exécute le bloc suivant.
+    if (sound) {
+      // Réinitialise le temps de lecture de l'audio à 0, permettant de rejouer le son depuis le début.
+      sound.currentTime = 0;
+      // Joue le fichier audio.
+      sound.play();
+      // Ajoute la classe 'playing' à l'élément 'key', déclenchant une animation définie dans le CSS.
+      key.classList.add('playing');
+      // Ajoute un écouteur d'événement pour l'événement 'transitionend' sur l'élément 'key'.
+      // Cet événement se déclenche lorsque la transition CSS (animation) sur l'élément est terminée.
+      key.addEventListener('transitionend', () => {
+        // Supprime la classe 'playing' de l'élément 'key' une fois l'animation terminée.
+        key.classList.remove('playing');
+      });
     }
-})
+  }
+});
